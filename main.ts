755c7a5fa0631bd5b98bd3537cf237f6260812a3
dommypooppy <domenic.villalobos32@bcpsd.org>
2024-11-20 08:47:54 -0600

    lol

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let mySprite = sprites.create(img`
+let variables = sprites.create(img`
     .......ffffffffffffffffff.......
     ......ffeeeeeeeeeeeeeeeeff......
     .....ffeeeeeeeeeeeeeeeeeeff.....
@@ -32,4 +32,35 @@ let mySprite = sprites.create(img`
     .cccccccccccccccccccccccccccccc.
     ..cccccccccccccccccccccccccccc..
     `, SpriteKind.Player)
-music.play(music.stringPlayable("E B C5 A B G A F ", 400), music.PlaybackMode.LoopingInBackground)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player))
+music.play(music.stringPlayable("E B C5 A B G A F ", 40), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSoundEffect(
+WaveShape.Sine,
+5000,
+1,
+255,
+0,
+200,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+), music.PlaybackMode.LoopingInBackground)
+scene.setBackgroundColor(7)
+info.changeScoreBy(2)
+controller.player2.moveSprite(variables, 0, 100)
